c86d0ac56bf4e3d5f6ba6b5cd950ab5d5729fb94
Alessio Biancalana <dottorblaster@gmail.com>
2019-05-08 19:36:21 +0200

    First commit

---
+++ lib/weather_api.ex
@@ -0,0 +1,18 @@
+defmodule WeatherApi do
+  @moduledoc """
+  Documentation for WeatherApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WeatherApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
